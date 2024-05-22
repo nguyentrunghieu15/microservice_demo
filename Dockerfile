1b97a2c77164cc0769c4b18f15a927a291f1c1a6
Hiro <nguyentrunghieugl15112002@gmail.com>
2024-05-23 02:18:58 +0700

    add bug treafik with swarm mode

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ COPY app.py .
 RUN pip install Flask
 
 # Expose the port (optional, comment out if not needed)
-#EXPOSE 5000
+EXPOSE 5000
 
 # Define the command to run the app
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app_id = str(uuid.uuid4())
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/hello-world')
 def hello_world():
   return "Hello, World!"+"app id:"+app_id
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0',port='8080')
+  app.run(host='0.0.0.0',port='5000')
